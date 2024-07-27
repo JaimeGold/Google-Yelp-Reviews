d20253f06b46303a2b5b9bb75fdc1fc4cda6f768
Jaime Gold <jaime.gold445@gmail.com>
2024-07-27 18:28:31 -0500

    solucion

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function convertNumber(num) {
+    if (typeof num !== 'number' || !Number.isInteger(num)) {
+      throw new Error('El argumento debe ser un número entero.');
     }
+    
+    if (num % 2 === 0) {
+      // Convertir a binario
+      return num.toString(2);
+    } else {
+      // Convertir a hexadecimal
+      return num.toString(16);
+    }
+  }
