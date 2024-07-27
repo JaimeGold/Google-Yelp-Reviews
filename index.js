dcbdd27a4701dd0c15265e0c56e5d216489231bf
Rocio <rolizetalaniz@gmail.com>
2024-07-27 20:49:58 -0300

    First coomit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+    if (numero % 2 === 0) {
+        // Convertir a binario y retornar
+        return numero.toString(2);
+    } else {
+        // Convertir a hexadecimal y retornar
+        return numero.toString(16);
     }
+}
+
+// Ejemplos de uso
+console.log(laCajaDePandora(4)); // "100" (4 en binario)
+console.log(laCajaDePandora(7)); // "7" (7 en hexadecimal)
