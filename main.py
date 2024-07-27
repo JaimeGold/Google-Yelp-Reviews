a4dfd4190da652acf36b87ffe552476bc3aca9b6
Thejairex <yairjesus777@gmail.com>
2024-07-27 20:51:54 -0300

    Clase and function

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ class person:
         self.name = "Yair Juarez"
         self.age = 20
         self.nation = "Argentina"
+        
+def convert_number(num):
+    if num % 2 == 0:  # Si el número es par
+        return bin(num)  # Convertir a binario
+    else:  # Si el número es impar
+        return hex(num)  # Convertir a hexadecimal
